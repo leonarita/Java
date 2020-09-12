a407f935e6eea9c9f2f5fa28ba63ba8470da779e
Leonardo <leo_narita@hotmail.com>
2020-09-12 18:55:46 -0300

    Testando referência

--- 1° Semestre/src/aula04/exercicio4/TesteReferencia.java
+++ 1° Semestre/src/aula04/exercicio4/TesteReferencia.java
@@ -2,12 +2,13 @@ package aula04.exercicio4;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 
 public class TesteReferencia {
 
 	public static void main (String[] args) {
 
-		// Para tipos de dados provenientes de classes a referência de memória não se aplica, seja por autoboxing ou por new
+		// Para tipos de dados provenientes de classes, a referência de memória não se aplicana atribuição (autoboxing por new)
 		// => Integer, Float, Long, Double, Character, Boolean, String, BigDecimal
 		
 		String st1 = "aaa";
@@ -19,6 +20,13 @@ public class TesteReferencia {
 		String st4 = st3;
 		st4 = "bbb";
 		imprimirDados(st3, st4);
+		
+		// Valor Monetário, Data, Hora => Atribuição por referência
+		Calendar dt1 = Calendar.getInstance();
+		dt1.set(2020, 07, 13, 02, 00);
+		Calendar dt2 = dt1;
+		dt2.set(2020, 07, 13, 12, 00);
+		imprimirDados(dt1.getTimeInMillis(), dt2.getTimeInMillis());
 		
 		// Para classes e coleções, a referência se aplica na atribuição
 		ArrayList<Integer> a = new ArrayList<>(Arrays.asList(2, 3));
