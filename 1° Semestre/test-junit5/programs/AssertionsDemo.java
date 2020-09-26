65a268533a32bda55b0fee27506793523981f995
Leonardo <leo_narita@hotmail.com>
2020-09-26 00:19:54 -0300

    Estudando assemptions

--- 1° Semestre/test-junit5/programs/AssertionsDemo.java
+++ 1° Semestre/test-junit5/programs/AssertionsDemo.java
@@ -1,5 +1,6 @@
 package programs;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -14,6 +15,14 @@ public class AssertionsDemo {
 		
 		assertEquals(3, 3, "It's done");
 		assertTrue(2==a, () -> "It's done again");
+	
+		int[] numbers = { 0, 1, 2, 3, 4 };
+		
+		assertAll("numbers", 
+			() -> assertEquals(numbers[0], 0),
+			() -> assertEquals(numbers[3], 3),
+			() -> assertEquals(numbers[4], 4)
+		);
 	}
 	
 	@Test
---
+++ 1° Semestre/test-junit5/programs/AssumptionDemo.java
@@ -0,0 +1,32 @@
+package programs;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AssumptionDemo {
+
+
+	@Test
+	void trueAssumption() {
+		assumeTrue(5>1);
+		assertEquals(5+2, 7);
+	}
+	
+	@Test
+	void falseAssumption() {
+		assumeFalse(5<1);
+		assertEquals(5+2, 7);
+	}
+	
+	@Test
+	void assumptionThat() {
+		String s = "Just a string";
+		assumingThat(s.equals("Just a string"), () -> assertEquals(2+2, 4));
+	}
+}
+
+// @Test, @ParameterizedTest, @RepeatedTest, @TestFactory, @TestTemplate, @TestMethodOrder
