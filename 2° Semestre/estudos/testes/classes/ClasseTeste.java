e16cf4b89e1a3cf69984d968dbba4241986501a2
Leonardo <leo_narita@hotmail.com>
2021-01-05 22:36:40 -0300

    Estudo de java

---
+++ 2° Semestre/estudos/testes/classes/ClasseTeste.java
@@ -0,0 +1,74 @@
+package testes.classes;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClasseTeste {
+	
+	private static class Classe {
+		private String nome;
+		private Long idade;
+		
+		public Classe(String nome, Long idade) {
+			this.nome = nome;
+			this.idade = idade;
+		}
+		
+	}
+	
+	private static List<Classe> classes = new ArrayList<>(Arrays.asList(new Classe("Teste", (long) 20), new Classe("Teste", (long) 20)));
+
+	public static void main(String[] args) {
+		
+        for(Object c : classes) {
+
+			Class<?> classe = c.getClass();
+			Field[] campos = classe.getDeclaredFields();
+			
+			String nomeAtributo = "";
+	        Object valorAtributo = null;
+	        String linha = "";
+                
+	        for (Field campo : campos) {            
+	        	try {               
+	            	nomeAtributo = campo.getName();
+	                campo.setAccessible(true);
+	                valorAtributo = campo.get(c);                           
+	            } catch (Exception e) {
+	                e.printStackTrace();
+	            }           
+	        	
+	        	linha += valorAtributo + ";";
+	            System.out.println(nomeAtributo + ": " + valorAtributo);            
+	        }
+	        
+	        System.out.println(linha);
+	        
+	        try {
+				write("teste.csv", linha);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	        
+	        linha = "";
+        }
+		
+	}
+	
+	public static void write(String path, String text) throws IOException {
+		
+		FileWriter fw = new FileWriter(path, true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write(text);
+		bw.newLine();
+
+		bw.close();
+	}
+
+}
---
+++ 2° Semestre/estudos/testes/fundamentos/Fundamentos.java
@@ -0,0 +1,10 @@
+package testes.fundamentos;
+
+public class Fundamentos {
+
+	public static void main(String[] args) {
+		String s = "Meu nome é %s %s %s";
+
+		System.out.printf(s, "Leonardo", "Naoki", "Narita");
+	}
+}
